b2ed8ccf40b12c5b8b1f45e095da139f710d5384
Alhuzwiri <alhuzwirialfi@gmail.com>
2023-07-20 22:39:54 +0700

    first commit

---
+++ src/javaapplication7/Windows/MainWindow.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication7.Windows;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author alhuzwiri
+ */
+public class MainWindow extends JFrame{
+    public MainWindow(){
+        this.setTitle("Aplikasi Port Scanning");
+        this.setSize(600,320);
+        
+    }
+}
